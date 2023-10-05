e41e61b6b7bdaadd4b4a9105d5a184f292848e23
ManmeetSingh777 <144683826+ManmeetSingh777@users.noreply.github.com>
2023-10-05 19:26:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const $team2score=document.getElementById("score-team2")
 const $team2wickets=document.getElementById("wickets-team2")
 const resetbutton=document.getElementById("reset")
 const strikebutton=document.getElementById("strike")
-const strike= new Audio(https://i1.faceprep.in/prograd-junior/bat%2Bhit%2Bball.mp3)
-const crowd_cheers= new Audio(https://i1.faceprep.in/prograd-junior/Ball%2BHit%2BCheer.mp3)
+const strike= new Audio("https://i1.faceprep.in/prograd-junior/bat%2Bhit%2Bball.mp3")
+const crowd_cheers= new Audio("https://i1.faceprep.in/prograd-junior/Ball%2BHit%2BCheer.mp3")
 
 var team1score = 0
 var team2score = 0
