bf384d48e0278aa8982c072b777c19769b8e4e07
ManmeetSingh777 <144683826+ManmeetSingh777@users.noreply.github.com>
2023-10-05 18:54:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const $team2score=document.getElementById("score-team2")
 const $team2wickets=document.getElementById("wickets-team2")
 const resetbutton=document.getElementById("reset")
 const strikebutton=document.getElementById("strike")
+const strike= new Audio(https://i1.faceprep.in/prograd-junior/bat%2Bhit%2Bball.mp3)
+const cheers= new Audio(https://i1.faceprep.in/prograd-junior/Ball%2BHit%2BCheer.mp3)
 
 var team1score = 0
 var team2score = 0
@@ -15,12 +17,15 @@ var ballsfaced=0
 var turn=1
 
 function finished(){
+    cheers.play();
     if(team1score>team2score) alert("India wins");
     if(team1score<team2score) alert("Pak wins");
+    if(team1score===team2score) alert("Draw");
 }
 
 const po=[0,1,2,3,4,5,6,"W"]
 strikebutton.onclick = () => {
+    strike.play();
     ballsfaced++;
      if(turn===1){
          var score = po[Math.floor(Math.random()* po.length)]
@@ -59,7 +64,7 @@ strikebutton.onclick = () => {
         $team2score.innerText = team2score
        }
        console.log(ballsfaced)
-       if(ballsfaced==6||team2wickets==2){
+       if(ballsfaced==6||team2wickets==2||team2score>team1score){
         turn=3
         finished()
     }
